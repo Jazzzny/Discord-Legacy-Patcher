60e668bd1cb3add0a00b7cff8124be23cdee7ab0
Jazzzny <jazzzny225@gmail.com>
2023-03-19 10:33:59 -0400

    Patch for 10.9 support

---
+++ support/patchbinary.py
@@ -0,0 +1,11 @@
+#Source: https://github.com/dortania/OpenCore-Legacy-Patcher/blob/main/Build-Binary.command#L303-L329
+print("Patching LC_VERSION_MIN_MACOSX")
+path = './dist/Discord\ Legacy\ Patcher'
+find = b'\x00\x0D\x0A\x00'
+replace = b'\x00\x09\x0A\x00'
+with open(path, 'rb') as f:
+    data = f.read()
+    data = data.replace(find, replace, 1)
+    with open(path, 'wb') as f:
+        f.write(data)
+print("Done")
